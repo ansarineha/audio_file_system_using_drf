b3c3860ba516e8a08cf148d46667b6795c1b066d
ansarineha <contactneha1498@gmail.com>
2021-05-09 14:49:36 +0530

    final commit

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\jaunel\desktop\neha\audioapi\env\scripts\python.exe
+#!c:\users\jaunel\desktop\audioapi\env\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
